7e34bd0ff34213f1839fceb3fba80f1c1f204ce9
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-05-27 20:44:59 -0400

    Add configuration example

---
+++ example/configure_example.py
@@ -0,0 +1,7 @@
+from dagurs039 import DaguRS039, data
+import time
+
+d = DaguRS039()
+d.cfg(data.motor_config['INDIV'], data.low_battery_voltage['2S'], 2, 2, 2, 2)
+time.sleep(1)
+d.encoder_cfg(13500, 800, 10, 10)
