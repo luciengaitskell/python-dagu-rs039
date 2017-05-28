1fef772794a854c4d7b7c8f7cf47a62c8037ffff
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-05-27 21:39:08 -0400

    Add motor drive example

---
+++ example/motor_drive_example.py
@@ -0,0 +1,19 @@
+from dagurs039 import DaguRS039, data
+import time
+
+d = DaguRS039()
+d.cfg(data.motor_config['INDIV'], data.low_battery_voltage['2S'], 2, 2, 2, 2)
+time.sleep(1)
+d.encoder_cfg(13500, 800, 10, 10)
+time.sleep(0.1)
+
+num_mtrs = 4
+for m in range(0, num_mtrs):
+    mp = [0] * num_mtrs
+    mp[m] = 10
+    print("Set Motor {} to 10".format(m))
+    d.set_mtr(*mp)
+    time.sleep(1)
+
+# Disable all motors:
+d.set_mtr(*([0]*num_mtrs))
