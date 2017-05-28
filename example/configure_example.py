aefcae3301333a2af54c881663558a67e830beb1
Lucien Gaitskell <lucien@gaitskell.com>
2017-05-28 13:22:46 -0400

    Fix sleep times during configuration writes

--- example/configure_example.py
+++ example/configure_example.py
@@ -3,5 +3,6 @@ import time
 
 d = DaguRS039()
 d.cfg(data.motor_config['INDIV'], data.low_battery_voltage['2S'], 2, 2, 2, 2)
-time.sleep(1)
+time.sleep(0.5)
 d.encoder_cfg(13500, 800, 10, 10)
+time.sleep(0.5)
--- example/motor_drive_example.py
+++ example/motor_drive_example.py
@@ -3,9 +3,9 @@ import time
 
 d = DaguRS039()
 d.cfg(data.motor_config['INDIV'], data.low_battery_voltage['2S'], 2, 2, 2, 2)
-time.sleep(1)
+time.sleep(0.5) # from tests I have found 0.2 also works, but this is for safety
 d.encoder_cfg(13500, 800, 10, 10)
-time.sleep(0.1)
+time.sleep(0.5)
 
 num_mtrs = 4
 for m in range(0, num_mtrs):
